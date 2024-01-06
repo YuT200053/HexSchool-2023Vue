c51ace419c533cc598f15f3e41ddcd1bd7b33c9b
YuTung Liu <yu.t.200053@gmail.com>
2024-01-06 21:57:43 +0800

    add login methods

--- js/login.js
+++ js/login.js
@@ -4,10 +4,33 @@ createApp({
   data() {
     return {
       user: {
-        username,
-        password,
+        username: "",
+        password: "",
       },
     };
   },
-  methods: {},
+  methods: {
+    login() {
+      const api = "https://vue3-course-api.hexschool.io/v2/admin/signin";
+
+      console.log(this.user);
+
+      axios
+        .post(api, this.user)
+        .then((res) => {
+          // 取得 data 中的 token 和到期日
+          const { token, expired } = res.data;
+          console.log(token, expired);
+          // 設定 Cookie
+          document.cookie = `yu-t-200053=${token};expires=${new Date(
+            expired
+          )};`;
+          //   登入成功就跳到產品頁
+          window.location = "product.html";
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    },
+  },
 }).mount("#app");
