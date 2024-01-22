7cea2d93ce0d35fe31b486ec3fe4f60538a60521
YuTung Liu <yu.t.200053@gmail.com>
2024-01-23 00:26:22 +0800

    w4 - finish login & add products.html

---
+++ w4/login.js
@@ -0,0 +1,36 @@
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+
+createApp({
+  data() {
+    return {
+      user: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    login() {
+      // 測試 console.log(this.user);
+      const api = "https://vue3-course-api.hexschool.io/v2/admin/signin";
+
+      axios
+        .post(api, this.user)
+        .then((res) => {
+          // 測試 console.log(res.data);
+          // 從 res.data 中取出 token 和 expired
+          const { token, expired } = res.data;
+          // 將 token & expired 存入 cookie 中
+          document.cookie = `yu-t-200053=${token}; expires=${new Date(
+            expired
+          )}`;
+          // console.log("登入成功");
+          // 登入成功就跳到 products.html
+          window.location = "products.html";
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    },
+  },
+}).mount("#app");
